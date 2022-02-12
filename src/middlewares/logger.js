7f7b949e07d4757ea67cc780b81155799cbd6151
Duilio Felix <duiliofelix@gmail.com>
2022-02-11 21:40:51 -0300

    fix: logger

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,7 +1,7 @@
 const logger = require('../services/logger');
 
 const loggerMidd = (req, res, next) => {
-  logger.info(req.method, req.baseUrl);
+  logger.info(`${req.method} - ${req.originalUrl}`);
   next();
 };
 
